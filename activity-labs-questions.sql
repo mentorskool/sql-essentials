eab74b531353ddc0c4847501d69783a6abae1275
Mentorskool <learn@mentorskool.com>
2022-04-08 01:21:39 +0530

    activity labs questions

---
+++ activity-labs-questions.sql
@@ -0,0 +1,83 @@
+-- Q1) How many sales transactions took place in Jun'16?
+-- List all the transactions as a table
+
+
+
+-- Q2) Show State wise profit. Output should be sorted by profit in decreasing order
+
+
+
+
+
+-- Q3) Show State wise profit. Output should be sorted by profit in decreasing order. Show top 5 states as the final output
+
+
+
+
+-- Q4) Show State wise profit margin. Profit Margin should be shown as percentage
+-- and should be rounded off to 2 decimal places. Show top 5 states as the final output
+
+
+
+-- Q5) Show sales by year and month and then 
+-- filter the result for last 3 months of 2017
+-- Output format : 
+-----------------------
+-- Month-Year | Sales 
+-----------------------
+-- Jan-2017 | 1000.25
+
+
+
+-- Q6) What change will you do in the query to fetch the same data as above
+-- for last 6 months and last 12 months?
+
+
+
+-- Q7) Now, what query will you write if
+--  all 3 results above needs to be appended together stacked vertically?
+-- Output format : 
+----------------------------
+-- Period         | Sales 
+----------------------------
+-- Last 3 Months  | 1000.25
+----------------------------
+-- Last 6 Months  | 1500.25
+----------------------------
+-- Last 12 Months | 1216.25
+----------------------------
+
+
+-- Q8) Calculate sales contribution% for each of the category by state
+-- Output format : 
+----------------------------------------
+-- Category         | Sales_Contribution
+----------------------------------------
+-- Technology       | 70.25%
+----------------------------------------
+-- Office Supplies  | 15.6%
+----------------------------------------
+-- Furniture        | 15.9%
+----------------------------------------
+
+
+
+-- Q9) Calculate customer behavior metrics as shown below : 
+-- Total spend by customer
+-- Total time active as a customer
+-- Total orders placed
+-- Average basket Value
+-- Average purchase frequency
+
+-- Output format : 
+---------------------------------------------------------------------------------------------------------------------------------
+-- CustomerID | Total_Amt_Spent  |  Total_Time_Active  | Total_Orders | Average Basket Value | Average purchase frequency
+----------------------------------------
+-- DK-1003    | 700.64           |
+----------------------------------------
+-- AA-1004    | 453.22           |
+----------------------------------------
+-- EM-1005    | 1523.76          |
+----------------------------------------
+
+
