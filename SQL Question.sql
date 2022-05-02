65b628b4d15feada6053759c7c658671384b143a
Mentorskool <learn@mentorskool.com>
2022-05-02 17:07:56 +0530

    questions

---
+++ SQL Question.sql
@@ -0,0 +1,44 @@
+-- Tables to be referred to : 
+-- cars_customers
+-- cars_policy
+-- cars_claims
+
+-- Q1) Find total number of accidents in 2018.
+-- 
+
+
+-- Q2) Find the number of accidents in where incident city is 'Riverwood' and incident_severity is 'Minor Damage' or 'Trivial Damage'.
+-- 
+
+
+-- Q3) What is the average account balance of those customes which are divorced in the Central region
+-- 
+
+
+-- Q4) Find the minium and maximum claim process time
+-- 
+
+
+-- Q5) Find avg policy annual premium by region
+-- 
+
+
+--Q6) Find total damage for the claims where multi vehicle side collision but claim rejected beause of no police_report available
+--	total damage = injury+property+vehicle
+--
+
+
+--Q7) Write down a query like this table shown below  
+-- which sort by 
+		--(1)region in ascending order 
+		--(2)policy_annual_premium in descending order 
+--|---------------------------------------------------------------------|
+--|policy_number|--car_id--|customer_id|--Region--|policy_annual_premium|
+--|-------------|----------|-----------|----------|---------------------|
+--|----230223---|--124249--|--159345---|--Central-|------1969.63--------|
+--|-------------|----------|-----------|----------|---------------------|
+
+--
+
+--Q8) Show year-month wise the count of Single Vehicle Collision
+--
